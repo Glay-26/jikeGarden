0060da4055cd5498171a6d88afc71e219df704c1
Glay-26 <844033898@qq.com>
2025-12-16 06:33:40 +0800

    echarts封装

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,10 @@
-import { useEffect } from "react"
-import { useRef } from "react"
-import * as echarts from 'echarts';
+import BarChart from "./components/barChart"
  
 const Home = () => {
-  const chartRef = useRef(null);
-  useEffect(() => {
-const chartDom = chartRef.current;
-const myChart = echarts.init(chartDom);
-const option = {
-  xAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      data: [120, 200, 150, 80, 70, 110, 130],
-      type: 'bar'
-    }
-  ]
-};
-
-option && myChart.setOption(option)
-  },[])
-  return <div>
-    <div ref={chartRef} style={{width: '500px', height: '400px'}} ></div>
-  </div>
+  return (<div>
+    <BarChart title={'Bar Chart111'}/>
+    <BarChart title={'Bar Chart222'}/>
+  </div>)
 }
 
 export default Home
